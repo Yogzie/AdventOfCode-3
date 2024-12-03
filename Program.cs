50f34b745f28fd39b18829d5e34fb689c3b0bd85
Yogett HD <14hodgettd@gmail.com>
2024-12-03 21:02:05 +0000

    Cleaned it up a bit

--- Program.cs
+++ Program.cs
@@ -14,15 +14,14 @@ namespace AdventOfCode__3
             Regex regExMul = new Regex(@"^mul\([0-9]{1,3},[0-9]{1,3}\).*");
 
             Regex regExDo = new Regex(@"^do\(\).*");
+
             Regex regExDont = new Regex(@"^don't\(\).*");
 
-            // Test String
-            // xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-            // Longest an argument can be is 12
-
-            int index = 0;
-            int maxLen = 12;
+            
+            int index = 0; 
+            int maxLen = 12; // This is the max length a string can be according to the rules
             int total = 0;
+
             bool ProcessMul = true;
             foreach (var c in memory)
             {
@@ -31,27 +30,30 @@ namespace AdventOfCode__3
 
                 if (regExDont.IsMatch(testString)) { ProcessMul = false; }
 
-
                 if (regExDo.IsMatch(testString)) { ProcessMul = true; }
-
 
                 // This matches Mul and will be processed
                 if (regExMul.IsMatch(testString) && ProcessMul)
                 {
-                    testString = testString.Split(')')[0];
-                    int num1;
-                    int num2;
-
-                    testString = testString.Split('(')[1];
-                    num1 = Convert.ToInt32(testString.Split(',')[0]);
-                    num2 = Convert.ToInt32(testString.Split(',')[1]);
-                    total = total + (num1 * num2);
+                    total = total + MultiplyMul(testString);
                 }
                 index++;
             }
 
             Console.WriteLine(total);
 
+        }
+        public static int MultiplyMul(string testString)
+        {
+            testString = testString.Split(')')[0];
+            int num1;
+            int num2;
+
+            testString = testString.Split('(')[1];
+            num1 = Convert.ToInt32(testString.Split(',')[0]);
+            num2 = Convert.ToInt32(testString.Split(',')[1]);
+
+            return (num1 * num2);
         }
     }
 }
