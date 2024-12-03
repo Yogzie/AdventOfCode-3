f085d86284a4faeecb5f816c1c45425ebc8705b6
Yogett HD <14hodgettd@gmail.com>
2024-12-03 19:44:52 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode__3
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Memory: ");
+
+            string memory = Console.ReadLine();
+            string rememberStr = "";
+
+            string regEx = "^[mul](+[0-9,]+)";
+
+            // xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+
+            int index = 0;
+            foreach (var c in memory)
+            {
+                
+                if (rememberStr == "mul(")
+                {
+                    Console.WriteLine(c);
+                }
+
+                index++;
+            }
+
+
+
+        }
+    }
+}
