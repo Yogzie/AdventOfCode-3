7dbe1d0f1b22a5323c75b4d73c364e6788fdf0f4
Yogett HD <14hodgettd@gmail.com>
2024-12-03 20:56:54 +0000

    Solved Part 1 and 2

--- Program.cs
+++ Program.cs
@@ -9,14 +9,12 @@ namespace AdventOfCode__3
         {
 
             string memory = File.OpenText(Environment.CurrentDirectory + "/input.txt").ReadToEnd();
-            string testStringMul;
-            string testStringDo;
-            string testStringDont;
+            string testString;
 
             Regex regExMul = new Regex(@"^mul\([0-9]{1,3},[0-9]{1,3}\).*");
 
             Regex regExDo = new Regex(@"^do\(\).*");
-            Regex regExDont = new Regex(@"^dont\(\).*");
+            Regex regExDont = new Regex(@"^don't\(\).*");
 
             // Test String
             // xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
@@ -25,22 +23,28 @@ namespace AdventOfCode__3
             int index = 0;
             int maxLen = 12;
             int total = 0;
+            bool ProcessMul = true;
             foreach (var c in memory)
             {
                 if ((Math.Abs(index - memory.Length)) < maxLen) { maxLen = memory.Length - index; }
-                testStringMul = memory.Substring(index, maxLen);
+                testString = memory.Substring(index, maxLen);
 
-                
-                
-                if (regExMul.IsMatch(testStringMul))
+                if (regExDont.IsMatch(testString)) { ProcessMul = false; }
+
+
+                if (regExDo.IsMatch(testString)) { ProcessMul = true; }
+
+
+                // This matches Mul and will be processed
+                if (regExMul.IsMatch(testString) && ProcessMul)
                 {
-                    testStringMul = testStringMul.Split(')')[0];
+                    testString = testString.Split(')')[0];
                     int num1;
                     int num2;
 
-                    testStringMul = testStringMul.Split('(')[1];
-                    num1 = Convert.ToInt32(testStringMul.Split(',')[0]);
-                    num2 = Convert.ToInt32(testStringMul.Split(',')[1]);
+                    testString = testString.Split('(')[1];
+                    num1 = Convert.ToInt32(testString.Split(',')[0]);
+                    num2 = Convert.ToInt32(testString.Split(',')[1]);
                     total = total + (num1 * num2);
                 }
                 index++;
